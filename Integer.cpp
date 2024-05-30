19eb8b72c8a5ec84346d12dc8ff0bf26a27bd81f
Seagate <139325612+Seagatemp@users.noreply.github.com>
2024-05-30 07:23:02 +0700

    Integer.cpp

---
+++ Integer.cpp
@@ -0,0 +1,22 @@
+include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    
+    cout <<" Masukkan Bilangan Bulat : ";
+    cin>>a;
+    cout <<"============================";
+    cout<<endl;
+
+
+        if(a >= 1){
+            cout << a << endl;
+        }
+        else{ 
+            cout << "(tidak ada)" << endl;
+        }
+
+    return 0;
+}
