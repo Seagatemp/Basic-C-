064939539993294fec4969d61eb4117587abfb9c
Seagate <139325612+Seagatemp@users.noreply.github.com>
2024-05-30 07:18:06 +0700

    Added_word.cpp

---
+++ Added word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    
+int main(){
+    string kalimat;
+    cout<<"Masukkan kalimat : ";
+    cin>>kalimat;
+    cout<<"Otomatis : ";
+    
+    cout<<kalimat<<" xixixi";cout<<endl;
+    return 0;
+}
